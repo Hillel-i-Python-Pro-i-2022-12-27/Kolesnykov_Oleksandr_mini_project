97daac503efc02c5c79f13b0cf917ff2f420e99c
oleksandr <sashkakoleso88@gmail.com>
2023-05-05 11:21:03 +0300

    refactored html templates

--- apps/products/views.py
+++ apps/products/views.py
@@ -2,14 +2,23 @@ from django.shortcuts import render
 
 
 def index(request):
+    context = {
+        "title": "Store",
+        "is_promotion": True,
+    }
     return render(
         request=request,
         template_name="index.html",
+        context=context,
     )
 
 
 def products(request):
+    context = {
+        "title": "Store - каталог",
+    }
     return render(
         request=request,
         template_name="products/products.html",
+        context=context,
     )
